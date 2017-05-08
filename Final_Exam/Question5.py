6195e31ae0ba3beb3e7cdd69c48c1d3d15bd7e4e
Zhiqi Mao <BH4DKC@users.noreply.github.com>
2017-05-07 22:25:41 -0500

    Create Question5.py

---
+++ Final_Exam/Question5.py
@@ -0,0 +1,14 @@
+def findBreak(sequence):
+  """
+  @Function: findBreak
+  @Description: find the first number that is not consecutive with in the list
+  @Returns: the first number that is not consecutive, if there is one. If not, return 'None'
+  """
+  x=sequence[0]      #set compare variable
+  for i in sequence:
+    if i == x:       #check consecutivity
+      x += 1         #set expected value for the next number
+    else:
+      return(i)      # return inconsecutive number
+      
+  return 'None'
